19c72bd71f6c70973e4ddef8eaeb5220d3a3d713
Sebastian B. Knapp <news@young-workers.de>
2018-09-10 14:05:09 +0200

    Release 0.078

--- lib/HO/mixin.pm
+++ lib/HO/mixin.pm
@@ -30,7 +30,7 @@
     ; $mix = [] unless ref $mix
     ; push @{$HO::accessor::classes{$class}}, @$mix
     ; my %acc = @$mix
-    ; my @methods = grep { !(/^new$/ || defined($acc{$_})) }
+    ; my @methods = grep { !(/^(init|new)$/ || defined($acc{$_})) }
         grep { ! $HO::class::class_methods{$mixin}{$_} }
         Package::Subroutine->findsubs( $mixin )
     ; Package::Subroutine->export_to($class)->($mixin,@methods)
@@ -39,3 +39,35 @@
 ; 1
 
 __END__
+
+=head1 NAME
+
+HO::mixin
+
+=head1 SYNOPSIS
+
+   package HOt::One;
+
+   use HO::class _rw => auto => '$';
+
+   package HOt::Dandy;
+
+   use HO::mixin 'HOt::One';
+   use HO::class;
+
+   HOt::Dandy->new->auto('matic');
+
+
+=head1 AUTHOR
+
+Mike Würfel, E<lt>sknpp@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2018 by p5-ho-developers
+
+You may distribute this code under the same terms as Perl itself.
+
+
+
+
